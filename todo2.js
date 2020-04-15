02632b329d67a4b4345fa864de255e98f72f0bcc
Beatyofcoding <60256215+Beatyofcoding@users.noreply.github.com>
2020-04-15 21:07:20 +0200

    Adding Enter(13)key and functioning withoutProblem

--- todo2.js
+++ todo2.js
@@ -11,10 +11,36 @@ const CHECK = "fa-check-circle";
 const UNCHECK = "fa-circle-thin";
 const LINE_THROUGH = "lineThrough";
 
-
-
 // Show today's date
 const options = {weekday : "long" , month:"short",day:"numeric"};
 const today = new Date();
 
 dateElement.innerHTML = today.toLocaleDateString("en-US", options );
+
+// add to do function
+
+function addToDo(toDo){
+
+  const item = `  <li class="item">
+                    <i class="fa fa-circles-thin co" job="complete" id="0"></i>
+                    <p class="text">${toDo}</p>
+                    <i class="fa fa-trash-o de" job="delete" id="0"></i>
+                    </li>
+                    `;
+                    
+      const position = "beforeend";
+
+  list.insertAdjacentHTML(position,item);
+}
+
+// add an item to the list user the enter key
+document.addEventListener("keyup",function(even){
+  if(event.keyCode == 13){
+       const toDo = input.value;
+
+       // if the input isn't empty
+       if(toDo){
+         addToDo(toDo);
+       }
+  }
+});
