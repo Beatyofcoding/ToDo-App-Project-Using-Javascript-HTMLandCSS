455762bc843a35104ad1696efbb50fbc8b9d773d
Beatyofcoding <60256215+Beatyofcoding@users.noreply.github.com>
2020-04-15 20:14:11 +0200

    ChangingCompletelyTheStructureOfTheCodeToBetterOne

--- todo2.js
+++ todo2.js
@@ -1,58 +1,20 @@
-const clear = document.querySelector('.clear')
-// what is a crud operation
-// c = create
-// r = read
-// u = update
-// d = delete
-let todos = [
-  { id: 1, text: 'Learn JavaScript', completed: true },
-  { id: 2, text: 'Seek for a job', completed: false },
-  { id: 3, text: 'Forget everything' }
-]
-// function showTodos() {
-//     let html = `
-//     <ul>
-//         <li>Learn JavaScriopt</li>
-//         <li>Seek fo a job</li>
-//         <li>Forget everything</li>
-//     </ul>`
-//     return html
-// }
-// instead of this we do as following
-function render() {
-  document.querySelector('ul').innerHTML = todos
-    .map(
-      (todo, index) => `<li class="${todo.completed ? 'done' : ''}">
-        <button onclick="handleRemove(${index})">x</button>
-        <span onclick="handleToggle(${index})">${todo.text}</span>
-        </li>`
-    )
-    .join('')
-}
-render()
-document.querySelector('form').onsubmit = function(event) {
-  event.preventDefault()
-  let id = generatedId()
-  let text = document.querySelector('input').value
-  let completed = document.querySelector()
-  let todo = { id, text }
-  todos.push(todo)
-  document.querySelector('input').value = ''
-  render()
-}
-//
-function generatedId() {
-  return todos.length === 0 ? 1 : todos[todos.length - 1].id + 1
-}
-function handleRemove(index) {
-  todos.splice(index, 1)
-  render()
-}
-function handleToggle(index) {
-  todos[index].completed = !todos[index].completed
-  render()
-}
-//////////////OR////////////////////////
-/// function generatedId() {
-// return !todos.length ? 1 : todos[todos.length - 1].id + 1;
-//};
+// Remaking everything in a more clean and clear way
+
+// Select the elements
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+
+// Classes names
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+
+
+
+// Show today's date
+const options = {weekday : "long" , month:"short",day:"numeric"};
+const today = new Date();
+
+dateElement.innerHTML = today.toLocaleDateString("en-US", options );
